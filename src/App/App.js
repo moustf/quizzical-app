2df9628226e9de2a889e0f0cb9c733630c2ec268
moustf <tepro.18@gmail.com>
2022-06-22 16:25:13 +0300

    Added the App.js file to render the compoents inside it close #5

---
+++ src/App/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Landing from "./Landing/Landing";
+
+export default function App() {
+  return (
+    <div className="container">
+      <Landing />
+    </div>
+  );
+}
